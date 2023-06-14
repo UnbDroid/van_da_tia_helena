6fe30e39de59d24617fb2f711bee7f7e9cd2e5b5
luisf <luisfelipehcarmona@gmail.com>
2023-06-14 15:27:52 -0300

    Novos modulos e atualização main luis

--- modules/choose.py
+++ modules/choose.py
@@ -1,5 +1,11 @@
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-import "motors.py"
+from modules.motors import *
+from modules.colors import *
+from modules.sensorUltrasonic import *
+
+def beggin_choosing():
+    moveFoward(40)
+    while not through_line():
 
--- modules/colors.py
+++ modules/colors.py
@@ -15,45 +15,19 @@ def getSensorColorRight():
 def getSensorColorLeft():
     return sensorColorLeft.rgb()
 
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------
-#Primeira possível solução
-
-blueRangeLeftR = [3, 4, 5, 6, 7]
-blueRangeLeftG = [13 ,14, 15, 16, 17]
-blueRangeLeftB = [62 ,63, 64, 65, 66]
-
-
-def isBlueLeft():
-    valueSensorColorLeft = getSensorColorLeft()
-
-    isBlueRangeR = valueSensorColorLeft[0] in blueRangeLeftR
-    isBlueRangeG = valueSensorColorLeft[1] in blueRangeLeftG
-    isBlueRangeB = valueSensorColorLeft[2] in blueRangeLeftB
-
-    return isBlueRangeR and isBlueRangeG and isBlueRangeB
-
-
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------
-#Segunda possível solução
-
-blueRangeRightMin = [3, 13, 20]
-blueRangeRightMax = [5, 15, 22]
-
-def isBlueRight():
-    valueSensorColorRight = getSensorColorRight()
-
-    isBlueRangeR = blueRangeRightMin[0] <= valueSensorColorRight[0] <= blueRangeRightMax[0]
-    isBlueRangeG = blueRangeRightMin[1] <= valueSensorColorRight[1] <= blueRangeRightMax[1]
-    isBlueRangeB = blueRangeRightMin[2] <= valueSensorColorRight[2] <= blueRangeRightMax[2]
-
-    return isBlueRangeR and isBlueRangeG and isBlueRangeB
-
-
 def printColorRight():
     return print(getSensorColorRight())
 
 
 def printColorLeft():
     return print(getSensorColorLeft())
+
+#-------------------------------------------------------------------------------
+#Passado pela linha
+
+
+def through_line():
+    if sensorColorLeft.reflection() < 10:
+        return True
+    else:
+        return False
