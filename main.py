1289983c2d2c1adcfafb761fd08407f3a1771d5e
luisf <luisfelipehcarmona@gmail.com>
2023-06-14 14:49:27 -0300

    atualização main

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env pybricks-micropython
-from modules.colors import *
-from modules.motors import *
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+sensor15 = UltrasonicSensor(Port.S1)
+sensor10 = UltrasonicSensor(Port.S4)
+motor = Motor(Port.A)
+
+# while(True):
+#     if (sensor15.distance() <= 100 and sensor10.distance() <= 100):
+#         print("Tamanho 15")
+#     elif(sensor10.distance() <= 100):
+#         print("Tamanho 10")
+#     else:
+#         print("Nada")
+# print('distancia 15 :', sensor15.distance() , 'distancia 10 :', sensor10.distance())
+# Write your program here.
+ev3.speaker.beep()
+
+
+def fechar():
+    while (Button.DOWN in ev3.buttons.pressed()):
+        motor.run(180)
+    motor.hold()
+
+
+def Abrir():
+    while Button.UP in ev3.buttons.pressed():
+        motor.run(-180)
+    motor.hold()
+
 
 while True:
-    moveFoward(100)
-    if isBlueRight():
-        moveStop()
-        break
+    fechar()
+    Abrir()
