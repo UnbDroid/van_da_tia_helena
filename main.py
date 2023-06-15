443d7a00ab5d22b486bb86b673d7bc23ca4a5b34
Pedro Henrique Alves Martos <phm.pedroalves@gmail.com>
2023-06-15 18:25:49 -0300

    Estudos Códigos

--- main.py
+++ main.py
@@ -8,53 +8,158 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
+
+#import time
+
+#classDriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+#class Color
+
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
-# Conexoes 
+# Create your objects here.
 ev3 = EV3Brick()
 
-# Conexões Motores:
-LeftMotor = Motor(Port.A)
-RightMotor = Motor(Port.B)
-Motores = DriveBase(LeftMotor,RightMotor,41, 113)
+# Portas dos Motores Left and right
+left_motor = Motor(Port.D)
+right_motor = Motor(Port.A)
 
-# Sensores: 
-LeftSensor = ColorSensor(Port.S1)
-RightSensor = ColorSensor(Port.S4)
 
-#       Algumas anotações:
-# Left Sensor : Branco = (36, 33, 94) Preto = (6, 6, 13) Vermelho = (31, 4, 5)
+# Touch sensor : meu botâo :D
+botao = TouchSensor(Port.S1)
 
-# Right Sensor: Branco = (24, 29, 31) Preto = (3, 3, 1)  Vermelho = (23, 3, 0)
+# Color sensor
+line_sensorR = ColorSensor(Port.S3)
+line_sensorL = ColorSensor(Port.S4)
 
-# Funções :
+# Ultrasonic Sensor
+olhos_sensor = UltrasonicSensor(Port.S2) 
 
-def RunWhite():
-    while LeftSensor.rgb()[2] > 89 and LeftSensor.rgb()[2] <100:
-        Motores.drive(200,0)
-        
-        
-def LeftRed():
-    while LeftSensor.rgb()[0] > 25 and LeftSensor.rgb()[0] < 39:
-        Motores.drive(200,0)
-        if LeftSensor.rgb()[2] > 8 and LeftSensor.rgb()[2] < 20:
-            Motores.drive(150,90)
+# Definindo Drivers Base para os motores (Whell diameter é o diametro da roda.) (Axle Track é as distâncias entre as duas rodas)
+motores = DriveBase(left_motor, right_motor, wheel_diameter=41, axle_track=108)
 
-# Alguns Testes.
+# Definindo as cores que o sensor identifica
+blackR = [5, 7, 5]
+whiteR = [70, 80, 88] #    Direito
+meioR = 44.5 # Definindo margem de erro
+
+blackL = [8, 16, 8]
+whiteL = [81, 100, 100 ] #    Esquerdo
+meioL = 54 # Definindo margem de erro para a esquerda
+
+
+
+redR = [59, 8, 5]  
+redL = [68, 16,7]
+
+# Definindo as médias para os sensores
+
+media1 = 63.5 # Media entre sensores, mas é uma boa arrumar isso futuramente
+media2 = 12
+media3 = 6
+
+cronometro = StopWatch()
+
+
+
 ev3.speaker.beep()
+motores.settings(straight_speed=200,turn_rate=120)
+# Parametros:
+#   straight_speed : Velocidade do robô 
+#   turn_rate : Velocidade de giro do robô
+
+
+apertou = False
+
+# Definindo funções:
+
+def VirarDireita():
+    return (line_sensorR.rgb()[2] < meioR and line_sensorL.rgb()[2] >= meioL)
+
+def VirarEsquerda():
+    return (line_sensorL.rgb()[2] < meioL and line_sensorR.rgb()[2] >= meioR)
+
+
 while True:
-    if (LeftSensor.rgb()[2] > 89 ) or (LeftSensor.rgb()[0] > 25 ):
-        Motores.drive(200,0)
+    if ((line_sensorR.rgb()[0] > 50 and line_sensorR.rgb()[1] <15)) and ((line_sensorL.rgb()[0] > 60 and line_sensorL.rgb()[1] < 25)):
+        ev3.speaker.beep()
+        motores.stop()
+        break
         
-    while LeftSensor.rgb()[2] > 8 and LeftSensor.rgb()[2] < 20:
-            Motores.drive(150,-200)
-            
-    while RightSensor.rgb()[0] > 0 and RightSensor.rgb()[2] < 10:
-            Motores.drive(150,200)
     
+    elif(line_sensorR.rgb()[2] >= meioR and line_sensorL.rgb()[2] >= meioL or line_sensorR.rgb()[2] <= meioR and line_sensorL.rgb()[2] <= meioL): #frente os dois
+        motores.drive(180, 0) #arrumar o tamanho
+        
+    elif VirarDireita():
+        motores.drive(40, 90) #DIREITA
+        
+    elif VirarEsquerda():
+        motores.drive(40, -90) #ESQUERDA
+        
+        
+    elif ((line_sensorR.rgb()[0] > 50 and line_sensorR.rgb()[1] <15)) or ((line_sensorL.rgb()[0] > 60 and line_sensorL.rgb()[1] < 25)):
+        ev3.speaker.beep()
+        motores.stop()
+        wait(5000)
+        ev3.speaker.beep()
+        
+    while (olhos_sensor.distance() <= 70):
+        
+        ev3.speaker.beep()
+        motores.turn(90)
+        motores.straight(150)
+        
+        motores.turn(-90)
+        motores.straight(360)
+        
+        motores.turn(-70)
+        while(line_sensorR.rgb()[2] >= meioR): 
+           motores.drive(140, 0) 
+        motores.turn(30)
+
+cronometro.reset()
+
+
+
+
+
+while cronometro.time() < 5000:
+    if botao.pressed():
+        apertou = True
+
+if apertou == True:
     
+    a  = True
+
+    motores.turn(-85)
+    motores.straight(140) 
+    motores.turn(90)
+    motores.straight(150) 
+    while(a):
+        ev3.speaker.beep(2)
+   
+                
+        
+else:  
+    a  = True
+   
+    motores.turn(90)
+    motores.straight(150) 
+    motores.turn(-90)
+    motores.straight(150) 
+    while(a):
+        ev3.speaker.beep(2)
+   
+                
+        
+        
     
+
     
-    
+        
+        
+       
+       
+        
+  
